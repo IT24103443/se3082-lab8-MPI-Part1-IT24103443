97232132075369d38605db1397a0596296443819
IT24103443 <it24103443@my.sliit.lk>
2026-09-24 12:06:23 +0530

    Update messages1.cc

--- messages1.cc
+++ messages1.cc
@@ -7,17 +7,22 @@ int main(void)
     MPI_Status status;
     MPI_Init(NULL, NULL);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    char name[30];
+
+    char name[MPI_MAX_PROCESSOR_NAME];
     int len;
     MPI_Get_processor_name( name, &len );
+    
     int x, y;
     x = 30;
+
     if (rank == 1) {
       printf("SEnding message to computer 3 from computer 1\n");
+      // Rank 1 sends to destination Rank 3
       MPI_Ssend(&x, 1, MPI_INT, 3, 0, MPI_COMM_WORLD);
     }
     else if (rank == 3) {
-      MPI_Recv(&y, 1, MPI_INT, 1, 0, MPI_COMM_WORLD, &status);
+      // Mismatch: Rank 3 expects a message from source Rank 2 (instead of Rank 1)
+      MPI_Recv(&y, 1, MPI_INT, 2, 0, MPI_COMM_WORLD, &status);
       printf("in computer 3 the value of y is %d\n", y);
     }
     else
